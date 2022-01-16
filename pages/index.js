c5ba59fdd5eaacac0b829d78311104fdd40e24f5
thangdx-1076 <dang.xuan.thang@sun-asterisk.com>
2022-01-17 00:21:35 +0700

    first commit :v

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+
+function Home() {
+  return (
+    <>
+      <h1>Next JS pre-rendering</h1>
+      <Link href='/users'>
+        <a>Users</a>
+      </Link>
+      <br></br>
+      <Link href='/posts'>
+        <a>Posts</a>
+      </Link>
+    </>
+  )
+}
+
+export default Home
